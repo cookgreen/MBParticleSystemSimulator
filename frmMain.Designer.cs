da41aa75ab8c8976c2c07c33e52bd7e98ec22e03
DoDoCat <dell20091234@gmail.com>
2022-07-15 14:13:23 +0800

    Initial Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace MBParticleSystemSimulator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabSettings = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnRunBySetting = new System.Windows.Forms.Button();
+            this.tabPYCode = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPythonCode = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtRunByPythonCode = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabSettings.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.tabPYCode.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(1209, 644);
+            this.splitContainer1.SplitterDistance = 620;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabSettings);
+            this.tabControl1.Controls.Add(this.tabPYCode);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(620, 644);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabSettings
+            // 
+            this.tabSettings.Controls.Add(this.tableLayoutPanel1);
+            this.tabSettings.Location = new System.Drawing.Point(4, 29);
+            this.tabSettings.Name = "tabSettings";
+            this.tabSettings.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSettings.Size = new System.Drawing.Size(612, 611);
+            this.tabSettings.TabIndex = 0;
+            this.tabSettings.Text = "Particle Settings";
+            this.tabSettings.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(606, 605);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnRunBySetting);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 528);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(600, 74);
+            this.panel3.TabIndex = 3;
+            // 
+            // btnRunBySetting
+            // 
+            this.btnRunBySetting.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRunBySetting.Location = new System.Drawing.Point(443, 14);
+            this.btnRunBySetting.Name = "btnRunBySetting";
+            this.btnRunBySetting.Size = new System.Drawing.Size(143, 44);
+            this.btnRunBySetting.TabIndex = 0;
+            this.btnRunBySetting.Text = "Run";
+            this.btnRunBySetting.UseVisualStyleBackColor = true;
+            this.btnRunBySetting.Click += new System.EventHandler(this.btnRunBySetting_Click);
+            // 
+            // tabPYCode
+            // 
+            this.tabPYCode.Controls.Add(this.tableLayoutPanel3);
+            this.tabPYCode.Location = new System.Drawing.Point(4, 29);
+            this.tabPYCode.Name = "tabPYCode";
+            this.tabPYCode.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPYCode.Size = new System.Drawing.Size(612, 611);
+            this.tabPYCode.TabIndex = 1;
+            this.tabPYCode.Text = "Python Code";
+            this.tabPYCode.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(606, 605);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPythonCode);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(600, 519);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Python Code";
+            // 
+            // txtPythonCode
+            // 
+            this.txtPythonCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPythonCode.Location = new System.Drawing.Point(3, 23);
+            this.txtPythonCode.Name = "txtPythonCode";
+            this.txtPythonCode.Size = new System.Drawing.Size(594, 493);
+            this.txtPythonCode.TabIndex = 0;
+            this.txtPythonCode.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtRunByPythonCode);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 528);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 74);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtRunByPythonCode
+            // 
+            this.txtRunByPythonCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRunByPythonCode.Location = new System.Drawing.Point(432, 18);
+            this.txtRunByPythonCode.Name = "txtRunByPythonCode";
+            this.txtRunByPythonCode.Size = new System.Drawing.Size(143, 44);
+            this.txtRunByPythonCode.TabIndex = 1;
+            this.txtRunByPythonCode.Text = "Run";
+            this.txtRunByPythonCode.UseVisualStyleBackColor = true;
+            this.txtRunByPythonCode.Click += new System.EventHandler(this.txtRunByPythonCode_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 644);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mount&Blade Particle System Simulator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabSettings.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.tabPYCode.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private TabControl tabControl1;
+        private TabPage tabSettings;
+        private TabPage tabPYCode;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel3;
+        private TableLayoutPanel tableLayoutPanel2;
+        private GroupBox groupBox1;
+        private RichTextBox txtPythonCode;
+        private Panel panel1;
+        private Panel panel3;
+        private Button btnRunBySetting;
+        private Button txtRunByPythonCode;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,20 @@
+namespace MBParticleSystemSimulator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRunBySetting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRunByPythonCode_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
